0af2311012e5e13b9e0a26af00a9337c04eec8dd
Kaweesi Matia <matiakaweesi@gmail.com>
2022-10-19 13:19:10 +0300

    create a new game ,create and save ID for the game,implement submit and refresh buttons

--- src/modules/arrayObject.js
+++ src/modules/arrayObject.js
@@ -1,32 +1,12 @@
-export const scores = [
-  {
-    name: 'kaweesi',
-    score: 75,
-  },
-  {
-    name: 'henry',
-    score: 60,
-  },
-  {
-    name: 'stanley',
-    score: 80,
-  },
-  {
-    name: 'william',
-    score: 65,
-  },
-  {
-    name: 'kevin',
-    score: 90,
-  },
-  {
-    name: 'daniel',
-    score: 33,
-  },
-];
-export const displayScores = (scores) => {
-  const marks = document.querySelector('.scores');
-  scores.forEach((score, index) => {
-    marks.innerHTML += `  <p class="${index % 2 !== 0 ? 'item' : 'item1'}">${score.name}:<span> ${score.score}</span></p>`;
+const scoredisplay = document.querySelector('.scores');
+
+export const displayScores = (allMembers) => {
+  const allUsers = (allMembers.result);
+  let items = '';
+  allUsers.forEach((allUsers, index) => {
+    items += `<p class="${index % 2 !== 0 ? 'item' : 'item1'}">${allUsers.user}:<span> ${allUsers.score}</span></p>`;
+    scoredisplay.innerHTML = items;
   });
 };
+
+export default displayScores;
